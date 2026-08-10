472ad80c0af8debe8172291078320b000f28fc13
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-08-10 14:06:56 +0000

    【基盤設定】enum設定・i18n日本語化適用完了

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,7 @@ class Request < ApplicationRecord
   # アソシエーション
   belongs_to :user
   has_one :notification, as: :notifiable, dependent: :destroy
+
+  # enum設定
+  enum :progress_status, { in_progress: 0, rejection: 1, added: 2 }
 end
