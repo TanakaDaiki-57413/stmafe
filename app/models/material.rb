edbccb5416d7f33e8453b974018622ed00f5a509
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-08-11 08:55:07 +0000

    【作成】トップページ 【導入】rubocop 【修正】rubocopを試しエラーだったファイル

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,6 @@
 class Material < ApplicationRecord
   # アソシエーション
-  has_many :reviews dependent: :destroy
+  has_many :reviews, dependent: :destroy
   has_many :material_tags
   has_many :tags, through: :material_tags
 
--- db/migrate/20260810082329_create_reviews.rb
+++ db/migrate/20260810082329_create_reviews.rb
@@ -5,7 +5,7 @@ class CreateReviews < ActiveRecord::Migration[8.1]
       t.references :material, null: false, foreign_key: true
       t.string :content, null: false
       t.integer :study_time, null: false
-      t.float :rate, null:false
+      t.float :rate, null: false
       t.timestamps
     end
   end
