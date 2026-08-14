7bcbcb0b930ab508eca94c15e22a0f819a9e17dc
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-08-14 10:12:07 +0000

    deploy setting

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Stmafe
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_support.use_solid_cache = false
   end
 end
