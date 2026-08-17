ad19cfaae424314a3af5577065a414cac80db64a
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-08-17 05:44:39 +0000

    【管理者機能】タグ機能作成

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,13 @@
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:submit-end", (event) => {
+  if (event.detail.success) {
+    const errorMessages = document.getElementById("error_messages")
+
+    if (errorMessages) {
+      errorMessages.remove()
+    }
+
+  }
+})
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,7 @@ class Tag < ApplicationRecord
   # アソシエーション
   has_many :material_tags
   has_many :materials, through: :material_tags
+
+  # バリデーション
+  validates :name, presence: true
 end
