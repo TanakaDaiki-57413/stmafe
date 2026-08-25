14e578d824ee2576f734eadedc3da2a62b8899fe
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-08-25 11:00:02 +0000

    【完了】通知機能

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,3 +1,17 @@
 class Public::ApplicationController < ApplicationController
   include Public::Authentication
+  before_action :set_header_notifications
+
+  private
+
+  def set_header_notifications
+    # 例: ログイン中のユーザーがいれば、その未読通知を取得する
+    if current_user
+      @notifications = current_user.notifications.where(is_read: false)
+                                                 .order(created_at: :desc)
+                                                 .limit(5)
+    else
+      @notifications = []
+    end
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,3 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "script"
-
-document.addEventListener("turbo:submit-end", (event) => {
-  if (event.detail.success) {
-    const errorMessages = document.getElementById("error_messages")
-
-    if (errorMessages) {
-      errorMessages.remove()
-    }
-
-  }
-})
